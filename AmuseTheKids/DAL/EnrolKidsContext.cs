7f4ff1aaf65c6181fdce84f61dd7255f29ee7778
clareshiels <clare.cashin@gmail.com>
2016-07-26 20:28:08 +0100

    Entity classes and db context class

---
+++ AmuseTheKids/DAL/EnrolKidsContext.cs
@@ -0,0 +1,20 @@
+﻿using AmuseTheKids.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Web;
+
+namespace AmuseTheKids.DAL
+{
+    public class EnrolKidsContext : DbContext
+    {
+        public EnrolKidsContext() : base("EnrolKidsContext")
+        {
+        }
+        public DbSet<Child> Children { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
